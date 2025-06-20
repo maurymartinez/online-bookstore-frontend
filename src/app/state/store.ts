ca6aeb3cdbf1a5f0051e148b142beb8dfbc4cebc
maury <maury.martinez@tutanota.com>
2025-06-20 10:09:06 +0200

    Add some missing ';'

--- src/app/state/store.ts
+++ src/app/state/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/slices/authSlice';
-import booksReducer from '../features/catalog/slices/booksSlice'
+import booksReducer from '../features/catalog/slices/booksSlice';
 
 export const store = configureStore({
     reducer: {
