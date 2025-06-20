7dfa9f893c40bda8e98e39f5ca4df938b99ae2fd
maury <maury.martinez@tutanota.com>
2025-06-20 12:46:18 +0200

    Fix login router

--- src/app/shared/router/RedirectIfAuthenticated.tsx
+++ src/app/shared/router/RedirectIfAuthenticated.tsx
@@ -1,9 +1,8 @@
 import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
+import { Navigate, Outlet  } from 'react-router-dom';
 import type { RootState } from '../../state/store';
-import type { JSX } from 'react';
 
-export default function RedirectIfAuthenticated({ children }: { children: JSX.Element }) {
-    const token = useSelector((state: RootState) => state.auth.token);
-    return token ? <Navigate to="/books" /> : children;
+export default function RedirectIfAuthenticated() {
+    const token = useSelector((state: RootState) => state.auth.token)
+    return token ? <Navigate to="/books" replace /> : <Outlet />
 }
