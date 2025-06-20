7ce59d8757d60cc1fe5ae6d256fab1551edded0a
maury <maury.martinez@tutanota.com>
2025-06-20 09:36:37 +0200

    remove unused status on authSlice

--- src/app/features/auth/slices/authSlice.ts
+++ src/app/features/auth/slices/authSlice.ts
@@ -5,13 +5,11 @@ import type { User, Credentials } from '../domain/types';
 export type AuthState = {
     user: User | null;
     token: string | null;
-    status: 'idle' | 'loading' | 'succeeded' | 'failed';
 };
 
 const initialState: AuthState = {
     user: null,
     token: null,
-    status: 'idle',
 };
 
 export const login = createAsyncThunk<
